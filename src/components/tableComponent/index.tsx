142343a284bf0667245d4e3ccecd6f336e5b5c66
trannhon70 <kevintran351996@gmail.com>
2025-02-05 14:22:30 +0700

    update

--- src/components/tableComponent/index.tsx
+++ src/components/tableComponent/index.tsx
@@ -1,7 +1,7 @@
 import type { PaginationProps } from 'antd';
 import { Pagination, Table } from 'antd';
 import { TableRowSelection } from 'antd/es/table/interface';
-import { FC, Fragment, useState } from 'react';
+import React, { FC, Fragment, useState, useCallback, useMemo, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 type TableProps = {
@@ -12,45 +12,57 @@ type TableProps = {
   pageIndex?: number;
   pageSize?: number;
   onChangePage?: any;
-  scroll?: any
+  scroll?: any;
 };
 
-const TableComponent: FC<TableProps> = (props) => {
-  const { columns, data, total, rowKey = false, pageIndex, pageSize, onChangePage ,scroll} = props;
-  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]); 
-  const {t } = useTranslation(['setting'])
+const TableComponent: FC<TableProps> = React.memo((props) => {
+  const { columns, data, total, rowKey = false, pageIndex, pageSize, onChangePage, scroll } = props;
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const { t } = useTranslation(['setting']);
+  const startRenderTime = performance.now();
   
-  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+  const onSelectChange = useCallback((newSelectedRowKeys: React.Key[]) => {
     console.log('Selected Row Keys changed: ', newSelectedRowKeys);
     setSelectedRowKeys(newSelectedRowKeys);
-  };
+  }, []);
 
-  const rowSelection: TableRowSelection<any> | undefined = rowKey
-  ? {
-      selectedRowKeys,
-      onChange: onSelectChange,
-    }
-  : undefined;
+  // useMemo để tránh tính toán lại object rowSelection
+  const rowSelection: TableRowSelection<any> | undefined = useMemo(
+    () =>
+      rowKey
+        ? {
+            selectedRowKeys,
+            onChange: onSelectChange,
+          }
+        : undefined,
+    [rowKey, selectedRowKeys, onSelectChange]
+  );
 
+  const showTotal: PaginationProps['showTotal'] = (total) =>
+    `${t('setting:tong')} ${total} ${t('setting:muc')}`;
 
-  const showTotal: PaginationProps['showTotal'] = (total) => `${t("setting:tong")} ${total} ${t("setting:muc")}`;
+  useEffect(() => {
+    const endRenderTime = performance.now();
+    console.log(`🕒 TableComponent render time: ${(endRenderTime - startRenderTime).toFixed(2)}ms`);
+  });
 
   return (
     <Fragment>
       <Table<any>
         columns={columns}
-        dataSource={data.length > 0 ? data : [] }
+        dataSource={data.length > 0 ? data : []}
         pagination={false}
         rowSelection={rowSelection}
-        rowKey={(record) => record.id} 
-        size='small'
+        rowKey={(record) => record.id}
+        size="small"
         scroll={scroll}
         bordered
+        sticky 
       />
       <Pagination
         className="mt-2"
         size="default"
-        align="end" 
+        align="end"
         showSizeChanger
         current={pageIndex}
         pageSize={pageSize}
@@ -61,6 +73,6 @@ const TableComponent: FC<TableProps> = (props) => {
       />
     </Fragment>
   );
-};
+});
 
 export default TableComponent;
