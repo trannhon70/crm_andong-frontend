f41bc52771e747e8bfc11f44b05ae7d651d2d504
trannhon70 <kevintran351996@gmail.com>
2026-03-16 15:25:53 +0700

    update

--- src/pages/patientAppointmentManagement/appointmentRegistrationList/index.tsx
+++ src/pages/patientAppointmentManagement/appointmentRegistrationList/index.tsx
@@ -85,7 +85,9 @@ const AppointmentRegistrationList: FC = () => {
                 userId: menu[1].ds.action_DSDKH.viewAllData ? '' : entities.id,
                 pageSize: pageSize,
                 pageIndex: pageIndex,
-                hospitalId: hospitalId
+                hospitalId: hospitalId,
+                //nếu user có id là 67 thì chỉ cho nhìn thấy danh sách FB2
+                mediaId: entities.id === 67 ? 22 : ""
             };
 
             setQuery(updatedQuery);
